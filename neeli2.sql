aa584ee6cd617a59092b3ed97f86383ce9fe65f6
nalleru <nalleru.neelima@gmail.com>
2022-07-06 17:02:13 +0530

    first commit

--- neeli2.sql
+++ neeli2.sql
@@ -43,6 +43,43 @@ left join product
 on order_details.ProductId = product.ProductId
 group by customer.CustomerName;*/
 
+/* 6. Fetch the Customer details, who has placed the first and last order*/
+/*select c.*, min(o.OrderedDate) as FirstOrder, max(o.OrderedDate) as LastOrder
+from customer c
+left join (
+select o.*,
+row_number() over(partition by CustomerId order by OrderedDate) as rn_asc,
+row_number() over(partition by CustomerId order by OrderedDate desc) as rn_desc
+from orders o
+) o on o.CustomerId= c.CustomerId
+where 1 in (o.rn_asc, o.rn_desc)
+group by c.CustomerId;*/
+
+/* 7.Fetch the customer details , who has placed more number of orders*/
+/*select customer.CustomerId, customer.CustomerName, count(order_details.OrderId) as NoOfOrders
+from customer
+left join orders
+on customer.CustomerId = orders.CustomerId
+left join order_details
+on orders.OrderId = order_details.OrderId
+group by customer.CustomerId
+order by NoOfOrders desc;*/
+
+/* 8. Fetch the customer details, who has placed multiple orders in the same year*/
+
+
+/* 9.Fetch the name of the month, in which more number of orders has been placed*/
+
+
+/* 10. Fetch the maximum priced Ordered Product*/
+/*select product.ProductName, product.ProductPrice
+from product
+right join order_details
+on product.ProductId = order_details.ProductId
+where product.ProductPrice in (
+select max(ProductPrice) from product);*/
+
+
 
 
 
